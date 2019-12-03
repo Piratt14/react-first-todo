3c927f0b403a35d1f8273d981c110e27f3b14edc
Piratt14 <satdvlad@gmail.com>
2019-12-03 09:34:08 +0300

    upgrade basic structure, added spread operator to code, used react items collection as array

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+
+    const elements = todos.map((item) => {
+
+        const { id, ...itemProps } = item;
+
+        return (
+            // { ... item } - использование spread оператора для объекта
+            // и одновременно с тем использование свойства
+            // { label: label } === { label
+            <li key={id}>
+                <TodoListItem { ...itemProps } />
+            </li>
+        );
+    });
+
     return (
         <ul>
-            <li><TodoListItem
-                label="Drink Coffee"/></li>
-            <li><TodoListItem
-                label="Create App"
-                important/></li>
+            { elements }
         </ul>
     );
 };
--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
 const App = () => {
+
+    const todoData = [
+        {label: 'Drink Coffee', important: false, id: 1},
+        {label: 'Make Awesome App', important: true, id: 2},
+        {label: 'Eat your dinner', important: false, id: 3},
+    ];
+
     return (
         <div>
             <AppHeader/>
             <SearchPanel/>
-            <TodoList/>
+            <TodoList todos={ todoData }/>
         </div>
     );
 };
