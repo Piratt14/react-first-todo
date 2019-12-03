56eddce8b538be2f07a7c42d35f0847bb15d0556
Piratt14 <satdvlad@gmail.com>
2019-12-03 21:30:52 +0300

    stop using state, get new props and pass it to event handlers

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,36 +4,12 @@ import './todo-list-item.css';
 
 export  default class TodoListItem extends Component{
 
-    state = {
-        done: false,
-        important: false
-    };
-
-    onLabelClick = () => {
-        this.setState(({ done }) => {
-            return {
-                done: !done
-            };
-        });
-    };
-
-    //тут используется прием с передачей не объекта в setState, а функции
-    //которая возвращает объект . Это говорит реакту вызывать функцию только
-    //когда состояние state будет финальным
-    //если новое состояние зависит от предыдущего состояния то обязательно
-    //нужно использовать функцию, а не просто объект
-    onMarkImportant = () => {
-        this.setState(({ important }) => {
-            return {
-                important: !important
-            };
-        });
-    };
-
     render() {
 
-        const { label, onDeleted } = this.props;
-        const { done, important } = this.state;
+        const { label, onDeleted,
+                onToggleImportant,
+                onToggleDone,
+                important, done } = this.props;
 
         let classNames = 'todo-list-item';
         if (done) classNames += ' done';
@@ -44,13 +20,13 @@ export  default class TodoListItem extends Component{
             <span className={classNames}>
                 <span
                     className="todo-list-item-label"
-                    onClick={ this.onLabelClick }>
+                    onClick={ onToggleDone }>
                     { label }
                 </span>
 
                 <button type="button"
                         className="btn btn-outline-success btn-sm float-right"
-                        onClick={ this.onMarkImportant }>
+                        onClick={ onToggleImportant }>
                     <i className="fa fa-exclamation"/>
                 </button>
 
