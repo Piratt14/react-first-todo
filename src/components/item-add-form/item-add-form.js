d69f2f78fb1cedb2ae5c80e26c22183f40bd30b7
Piratt14 <satdvlad@gmail.com>
2019-12-03 22:49:08 +0300

    add new stylys to form and create input element, create function that send user input to main component

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,17 +3,37 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+
+    state = {
+        label: '',
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdded(this.state.label);
+    };
+
     render() {
 
         const { onAdded } = this.props;
 
         return (
-            <div className="item-add-form">
-                <button className="btn btn-outline-secondary"
-                        onClick={ () => onAdded('hello') }>
-                    Add Element
+            <form className="item-add-form d-flex"
+                    onSubmit={this.onSubmit}>
+                <input type="text"
+                        className="form-control"
+                        onChange={this.onLabelChange}
+                        placeholder="What needs to be done"/>
+                <button className="btn btn-outline-secondary">
+                    Add item
                 </button>
-            </div>
+            </form>
         );
     }
 }
