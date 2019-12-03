debf03f38fea91e73b739a26e767c9b20cdd490e
Piratt14 <satdvlad@gmail.com>
2019-12-03 23:56:50 +0300

    improved the behavior of the function, added checking for an empty string, added two-way binding

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -16,20 +16,22 @@ export default class ItemAddForm extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
+        if (!this.state.label) return;
         this.props.onAdded(this.state.label);
+        this.setState({
+            label: ''
+        });
     };
 
     render() {
-
-        const { onAdded } = this.props;
-
         return (
             <form className="item-add-form d-flex"
                     onSubmit={this.onSubmit}>
                 <input type="text"
                         className="form-control"
                         onChange={this.onLabelChange}
-                        placeholder="What needs to be done"/>
+                        placeholder="What needs to be done"
+                        value={this.state.label}/>
                 <button className="btn btn-outline-secondary">
                     Add item
                 </button>
