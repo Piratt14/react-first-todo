c61b522dd088936fe36f2df8ec7e1e1d27dc749c
Piratt14 <satdvlad@gmail.com>
2019-12-03 21:28:35 +0300

    pass new props and custom event handler to each list item

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,7 +3,9 @@ import React from 'react';
 import TodoListItem from "../todo-list-item";
 import './todo-list.css';
 
-const TodoList = ({ todos, onDeleted }) => {
+const TodoList = ({ todos, onDeleted,
+                  onToggleImportant,
+                  onToggleDone}) => {
 
     const elements = todos.map((item) => {
 
@@ -16,7 +18,9 @@ const TodoList = ({ todos, onDeleted }) => {
             <li key={id} className="list-group-item">
                 <TodoListItem
                     { ...itemProps }
-                    onDeleted={ () => onDeleted(id) }/>
+                    onDeleted={ () => onDeleted(id) }
+                    onToggleImportant={ () => onToggleImportant(id) }
+                    onToggleDone={ () => onToggleDone(id) }/>
             </li>
         );
     });
