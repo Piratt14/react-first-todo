1a18f1f28be8ce6c289f0e92789c533faace0fc4
Piratt14 <satdvlad@gmail.com>
2019-12-03 17:25:07 +0300

    added an event handler to remove the list item

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,7 +3,7 @@ import React from 'react';
 import TodoListItem from "../todo-list-item";
 import './todo-list.css';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted }) => {
 
     const elements = todos.map((item) => {
 
@@ -14,7 +14,9 @@ const TodoList = ({ todos }) => {
             // и одновременно с тем использование свойства
             // { label: label } === { label
             <li key={id} className="list-group-item">
-                <TodoListItem { ...itemProps } />
+                <TodoListItem
+                    { ...itemProps }
+                    onDeleted={ () => onDeleted(id) }/>
             </li>
         );
     });
